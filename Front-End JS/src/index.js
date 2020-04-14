629b57358b65b83fe26326c3c5ce4fdb41b08a6d
Leon Jamison <leonjamison@icloud.com>
2020-04-14 13:57:50 -0400

    Timer working and merge2

--- Front-End JS/src/index.js
+++ Front-End JS/src/index.js
@@ -4,28 +4,37 @@ const playBttn = document.getElementById('play-button')
 let counter = document.getElementById('game-clock').textContent
 let clockDiv = document.getElementById("game-clock-div")
 // console.log(counter)
-// console.log(submitBttn)
 const wordListUrl = "http://localhost:3000/words"
 
+
 document.addEventListener('DOMContentLoaded', function (){
-    console.log("LETS SAVE THESE BEES!!")
+    console.log("LETS KILL THESE BEES!!")
 
     document.addEventListener('click', (e) => {
         e.preventDefault()
      // console.log(e.target)
-            if (e.target === submitBttn){
-                console.log("KILL THEM ALL!")
-                let player = userName.value
-                    playBttn.disabled = false
+     if (e.target === submitBttn){
+         console.log("KILL THEM ALL!")
+         let player = userName.value
+            playBttn.disabled = false
 
-            }else if (e.target === userName){
-                    console.log('Enter Username')
-            
-            }else {
-                console.log('ERRRORRRR!!!')
-            }
-        }    
-    )   
+     }else if(e.target === userName){
+            console.log('Enter Username')
+     
+     }else if(e.target === playBttn){
+        
+        let countdown = setInterval(function(){
+        counter--;
+        (counter == 1) ? document.getElementById("plural").textContent = "" : document.getElementById("plural").textContent = "s"
+        document.getElementById("game-clock").textContent = counter
+        if (counter <= 0) clearInterval(countdown)
+        },1000);
+
+        playBttn.textContent = "PAUSE"
+        console.log('LETS GET STARTED!')
+     }
+    })
+
 })
 
 function fetchWordList() { 
