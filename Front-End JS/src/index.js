7f1d328a76f4b3a9f0ee41e8ebb36d0cf26528c6
theslickestnick <nick.echev@gmail.com>
2020-04-14 11:45:10 -0400

    added fetch fucntion for words

--- Front-End JS/src/index.js
+++ Front-End JS/src/index.js
@@ -2,31 +2,34 @@ const submitBttn = document.getElementById('login-submit')
 const userName = document.getElementById('login-field')
 const playBttn = document.getElementById('play-button')
 // console.log(submitBttn)
+const wordListUrl = "http://localhost:3000/words"
 
 document.addEventListener('DOMContentLoaded', function (){
-    console.log("LETS KILL THESE BEES!!")
+    console.log("LETS SAVE THESE BEES!!")
 
     document.addEventListener('click', (e) => {
         e.preventDefault()
      // console.log(e.target)
-     if (e.target === submitBttn){
-         console.log("KILL THEM ALL!")
-         let player = userName.value
-            playBttn.disabled = false
+            if (e.target === submitBttn){
+                console.log("KILL THEM ALL!")
+                let player = userName.value
+                    playBttn.disabled = false
 
-     }else if(e.target === userName){
-            console.log('Enter Username')
-     
-     }else {
-         console.log('ERRRORRRR!!!')
-     }
+            }else if (e.target === userName){
+                    console.log('Enter Username')
+            
+            }else {
+                console.log('ERRRORRRR!!!')
+            }
+        }    
+    )   
+})
+
+function fetchWordList() { 
+    fetch(wordListUrl) 
+        .then(response => response.json())
+        .then(words => console.log(words))
     }
 
-    
-    )
-
-
-   
-
-})
+fetchWordList()
 
